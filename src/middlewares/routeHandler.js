cdaa480c69106553b12f686edab4ec4f90d13a28
Wagnetto <wagnetto@protonmail.com>
2025-02-07 11:29:14 -0300

    Extract query params

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,15 +1,20 @@
 import { Database } from '../database/database.js'
 import { routes } from '../routes/index.js'
+import { extractQueryParams } from '../utils/extractQueryParams.js'
 
 const database = new Database()
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    console.log(route)
     return route.method === request.method && route.path.test(request.url)
   })
 
   if (route) {
+    const routeParams = request.url.match(route.path)
+    const { query } = routeParams.groups
+
+    request.query = query ? extractQueryParams(query) : {}
+
     return route.controller({ request, response, database })
   }
 
